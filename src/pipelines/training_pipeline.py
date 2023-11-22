1004f194ee2f634a67fdce5ed1faf086066972df
Ashish Kumar Jha <76522103+Ashu-1399@users.noreply.github.com>
2023-11-22 22:41:45 +0530

    update 22 NOV

--- src/pipelines/training_pipeline.py
+++ src/pipelines/training_pipeline.py
@@ -11,10 +11,10 @@ from src.components.model_trainer import ModelTrainer
 if __name__ == "__main__":
     obj = DataIngestion()
     train_data_path, test_data_path = obj.initiate_data_ingestion()
-    print(train_data_path,test_data_path)
+    # print(train_data_path,test_data_path)
     data_transformation = DataTransformation()
     train_arr, test_arr, preprocessor_pkl_path = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
-    print(preprocessor_pkl_path)
+    # print(preprocessor_pkl_path)
 
     model_trainer = ModelTrainer()
     model_trainer.initate_model_training(train_arr, test_arr)
